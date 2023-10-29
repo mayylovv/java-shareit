3c89d38c6a1a40e085b2ebf34e36701542c514d2
mayylovv <119136126+mayylovv@users.noreply.github.com>
2023-10-29 22:43:17 +0300

    незначительные правки v2

--- src/main/java/ru/practicum/shareit/user/UserStorageInMemory.java
+++ src/main/java/ru/practicum/shareit/user/UserStorageInMemory.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.BadRequestException;
-import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.user.model.User;
 
