397ce5255cdf358f6c6ce5fd579f3b1c8a79b27b
mayylovv <119136126+mayylovv@users.noreply.github.com>
2023-12-11 02:36:46 +0300

    16 спринт 1 попытка

--- server/src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -2,17 +2,11 @@ package ru.practicum.shareit.item;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.PageRequest;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.UserRepository;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import ru.practicum.shareit.user.model.User;
 
 @DataJpaTest
 class ItemRepositoryTest {
